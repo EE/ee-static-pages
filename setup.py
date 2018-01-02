117328a95d3dff21e832e5a419a89cc5f405b1d9
Tadek Teleżyński <tadekte@gmail.com>
2018-01-02 10:38:37 +0100

    Change django dependency to "1.11.*"

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description='Django app which provides static pages functionality.',
     author='Laboratorium EE',
     packages=['ee_static_pages'],
-    install_requires=['django==1.11'],
+    install_requires=['django==1.11.*'],
     dependency_links=['https://github.com/EE/ee-seo-mixin/tarball/master#egg=ee-seo-mixin'],
 )
