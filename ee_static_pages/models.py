fa6e280802a59453f74d7cfe7abcedc96a28d67d
Tadek Teleżyński <tadekte@gmail.com>
2018-02-19 15:48:25 +0100

    Remove auto_slug saving, add absolute_url method and make slug non nullable

--- ee_static_pages/models.py
+++ ee_static_pages/models.py
@@ -1,5 +1,5 @@
+from django.core.urlresolvers import reverse
 from django.db import models
-from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
 
 from ee_seo_mixin.models import SearchEngineOptimizableEntity
@@ -12,27 +12,15 @@ class StaticPage(SearchEngineOptimizableEntity):
     slug = models.SlugField(
         max_length=140,
         unique=True,
-        null=True,
-        blank=True,
-        help_text=_('"Slug" is a part of an url adress. \
-                    If you leave it blank, it will be generated \
-                    automatically from the page\'s name.'))
+        help_text=_('"Slug" is a part of an url adress.'))
     content = models.TextField(
         verbose_name=_("Content"))
 
     def __str__(self):
         return self.name
 
-    def _get_unique_slug(self):
-        slug = slugify(self.name)
-        unique_slug = slug
-        num = 1
-        while StaticPage.objects.filter(slug=unique_slug).exists():
-            unique_slug = '{}-{}'.format(slug, num)
-            num += 1
-        return unique_slug
+    def get_absolute_url(self):
+        return reverse('static-page', kwargs={'slug': self.slug})
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = self._get_unique_slug()
         super(StaticPage, self).save(*args, **kwargs)
